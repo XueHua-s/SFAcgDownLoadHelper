c530bb174fd44449f4fd23897eff21648232fbb8
litleSnow <lijiaxin@xhblog.top>
2024-06-28 18:02:29 +0800

    feature: 提交进行显示插件的逻辑

--- src/contentScript/Content.vue
+++ src/contentScript/Content.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import { NPopover } from 'naive-ui'
+import { NPopover, useMessage } from 'naive-ui'
+import { computed } from 'vue'
 import SFGrilFloaDragButton from '~/components/SFGrilFloaDragButton.vue'
 import DownLoadHelper from '~/contentScript/components/DownLoadHelper.vue'
+import { sfHost, sfMHost } from '~/const/shifferHost'
+const message = useMessage()
+const isRender = computed(() => {
+  if (location.host === sfHost) {
+    return true
+  } else if (location.host === sfMHost) {
+    message.error('插件暂不支持在移动端进行下载')
+  }
+  return false
+})
 </script>
 
 <template>
-  <div class="content">
+  <div v-if="isRender" class="content">
     <NPopover placement="left-start" trigger="click">
       <template #trigger>
         <SFGrilFloaDragButton :size="60" />
--- src/contentScript/components/DownLoadHelper.vue
+++ src/contentScript/components/DownLoadHelper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="download"></div>
+  <div class="download">下载小助手</div>
 </template>
 <script setup></script>
 <style lang="scss" scoped></style>
